1023d06bf167979341bc6cfacd7a17dc50a48b97
Rodrigo Iván Ordoñez Chávez <rodrigoordonez@nube.unadmexico.mx>
2024-09-01 14:13:17 -0500

    Update test_colisiones.py

--- colisiones/test_colisiones.py
+++ colisiones/test_colisiones.py
@@ -2,13 +2,16 @@ import unittest
 from colisiones import f
 
 class TestColisiones(unittest.TestCase):
-    
+    """
+    Test class for function `f` of module `collisions`.
+    Use the `unittest` module to define and execute test cases.
+    """
     def test_cases(self):
         self.assertEqual(f('LR'), '0 0')
         self.assertEqual(f('RL'), '1 1')
         self.assertEqual(f('RRR'), '0 0 0')
         self.assertEqual(f('RRL'), '1 2 1')
-        # Agrega más casos de prueba aquí
+        # Add more test cases here
 
 if __name__ == '__main__':
     unittest.main()
